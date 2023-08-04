8006ce80673f94c4cc89ffdc570552c7181afa7a
Basit Mustafa <basit.mustafa@gmail.com>
2023-08-04 14:23:51 -0600

    fix(helpers): fix undefined var in exception handler

--- helpers.py
+++ helpers.py
@@ -342,7 +342,7 @@ def convert_pvc_to_simpler_dict(pvc):
     try:
         if 'volume.autoscaler.kubernetes.io/scale-up-max-size' in pvc.metadata.annotations:
             return_dict['scale_up_max_size'] = int(pvc.metadata.annotations['volume.autoscaler.kubernetes.io/scale-up-max-size'])
-    except:
+    except Exception as e:
         print("Could not convert scale_up_max_size to int: {}".format(e))
 
     try:
